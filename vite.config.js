8f56b70a0df92b9b867082a55cc3cafac901b09c
OscarCarranza5573 <alessandrocarranzaorellana0@gmail.com>
2025-08-29 22:23:40 -0600

    CAMBIO DE VITE

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/TAREA-CINE/', //  nombre del repo
+  base: '/mi-tarea-de-cine/', //  nombre del repo
 })
